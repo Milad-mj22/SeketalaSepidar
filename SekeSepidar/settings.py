ec21efd6a1128180a32a1a7ab4a8ee358657ba0f
milad <m.moltaji@yahoo.com>
2026-07-23 12:33:16 +0330

    nothing

--- SekeSepidar/settings.py
+++ SekeSepidar/settings.py
@@ -198,8 +198,8 @@ PUBLIC_KEY_FILE = 'public.pem'
 # روش دوم: استفاده از تنظیمات جداگانه (اختیاری)
 SQL_USE_CONNECTION_STRING = False  # اگر True باشد از string بالا استفاده می‌کند
 SQL_SERVER = 'DESKTOP-JKDSDCN\SEPIDAR'
-# SQL_SERVER = "DESKTOP-OCIN559"
-SQL_SERVER = "DESKTOP-HDSP3L0"
+SQL_SERVER = "DESKTOP-OCIN559"
+# SQL_SERVER = "DESKTOP-HDSP3L0"
 SQL_DATABASE = 'Sepidar01'
 SQL_DRIVER = '{ODBC Driver 17 for SQL Server}'
 SQL_TRUSTED_CONNECTION = 'yes'
